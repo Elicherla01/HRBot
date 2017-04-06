a7c6a25e97c8c9f5da4f76e658692eb0477df470
Ravindra Prasad <ravindraprasad@yahoo.com>
2017-04-06 16:16:04 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,86 +1,131 @@
 import os
-import sys
+import traceback
 import json
-
 import requests
+
 from flask import Flask, request
+
+from messages import get_message, search_keyword
+
+token = os.environ.get('ACCESS_TOKEN')
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
-
-
-@app.route('/', methods=['POST'])
-def webhook():
-
-    # endpoint for processing incoming messaging events
-
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
-    if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "got it, thanks!")
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-
-    return "ok", 200
-
-
-def send_message(recipient_id, message_text):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": os.environ["ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
+def send_attachment(sender, type, payload):
+    return {
         "recipient": {
-            "id": recipient_id
+            "id": sender
         },
         "message": {
-            "text": message_text
+            "attachment": {
+                "type": type,
+                "payload": payload,
+            }
         }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
+    }
 
 
-def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
-    sys.stdout.flush()
+def send_text(sender, text):
+    return {
+        "recipient": {
+            "id": sender
+        },
+        "message": {
+            "text": text
+        }
+    }
 
+
+def send_message(payload):
+    requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+
+
+def send_hr_info(sender, **kwargs):
+
+    r = requests.get(hr_text)
+    response = r.json()
+
+    print(response)
+
+    if 'cod' in response:
+        if response['cod'] != 200:
+            return 'error'
+
+                        
+
+    payload = send_attachment(sender,
+                              'template',
+                              {
+                                  "template_type": "list",
+                                  "top_element_style": "large",
+                                  "elements": elements,
+                                  "buttons": [
+                                      {
+                                          "title": "Weather",
+                                          "type": "postback",
+                                          "payload": "do_it_again"
+                                      }
+
+                                  ]
+                              })
+
+    payload1 = send_attachment(sender,
+                              'template',
+                              {
+                                 "template_type":"button",
+                                 "text":"What do you want to do next?",
+                                 "buttons":[
+                                    {
+                                        "type":"web_url",
+                                        "url":"https://www.tesco.com",
+                                        "title":"Show Website"
+                                    },
+                                    {
+                                        "type":"postback",
+                                        "title":"weather",
+                                        "payload":"do_it_again"
+                                    }
+                                    ]
+                                   })
+
+
+
+    send_message(payload)
+    return None
+
+
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.data.decode())
+            sender = data['entry'][0]['messaging'][0]['sender']['id']
+
+            print(data)
+
+            if 'message' in data['entry'][0]['messaging'][0]:
+                message = data['entry'][0]['messaging'][0]['message']
+                text = message['text']
+                
+                chat_message = search_keyword(text)
+
+                if chat_message:
+                    # if found keyword, reply with chat stuff
+                    message = send_text(sender, get_message('greetings'))
+                    send_message(message)
+                else:
+                    message = send_text(sender, get_message('greetings'))
+                    send_message(message)
+
+        except Exception as e:
+            print(traceback.format_exc())
+    elif request.method == 'GET':
+        if request.args.get('hub.verify_token') == os.environ.get('VERIFY_TOKEN'):
+            return request.args.get('hub.challenge')
+        return "Wrong Verify Token"
+    return "Nothing"
 
 if __name__ == '__main__':
     app.run(debug=True)
