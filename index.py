fbe47c00ca7e991ad5bf2bfb8d180737099d2180
Ravindra Prasad <ravindraprasad@yahoo.com>
2017-04-19 14:12:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -140,17 +140,17 @@ def webhook():
 				message = send_text(sender, hr_message)
 				send_message(message)
 
-   		   	    if name_text == 'greet':
+   		   	    elif name_text == 'greet':
 				hr_message="Glad to met you"
 				message = send_text(sender, hr_message)
 				send_message(message)	
 				
-			    if name_text == 'OJP_approval':
+			    elif name_text == 'OJP_approval':
 				hr_message="Your line manager endorsement is required for you to apply for the role. You must discuss in detail the role requirement (JD), the skill requirement (Skill matrix) and your career aspirations, with your manager. During this discussion your manager will help you identify skill gaps and work on developing your personal development plan that will help you to achieve your aspirational role. If your manager and you agree that you have all the skills required for the role applied for, then your manager will provide the required endorsement."
 				message = send_text(sender, hr_message)
 				send_message(message)
 			    
-			    if name_text == 'OJP_search':
+			    elif name_text == 'OJP_search':
 				hr_message="The Open Job will be posted as and when vacancies are identified and agreed to be released."
 				message = send_text(sender, hr_message)
 				send_message(message)	
